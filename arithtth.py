de7efa4eac041f9feae51cd5e079cff888d2c2e1
gopalakrishnanngk <34596624+gopalakrishnanngk@users.noreply.github.com>
2018-10-14 23:37:56 +0530

    Update and rename arithtth.py to arithtthsum.py

--- arithtth.py
+++
@@ -1,8 +0,0 @@
-def arithsum(n,a,d):
-	sum=(n/2)*(2*a+(n-1)*d)
-	return sum
-n=int(input())
-a=int(input())
-d=int(input())
-print(n, a, d)
-print(int(arithsum(n,a,d)))
---
+++ arithtthsum.py
@@ -0,0 +1,9 @@
+s=input()
+a=s.split()
+n=int(a[0])
+b=int(a[1])
+d=int(a[2])
+sum=(n/2)*(2*b+(n-1)*d)
+print(int(sum))
+	
+				
