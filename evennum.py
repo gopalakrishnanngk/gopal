54667c586f4aa3c6bafd83067e2415fe89f7d937
gopalakrishnanngk <34596624+gopalakrishnanngk@users.noreply.github.com>
2018-10-14 21:51:38 +0530

    Update evennum.py

--- evennum.py
+++ evennum.py
@@ -1,9 +1,6 @@
-a=int(input())
-
-b=int(input())
-
-evens=[str(x) for x in range(a+1,b-1) if x%2==0]
-
-odds=[str(x) for x in range(a+1,b) if x%2==1]
-
-print("\t"+"\t".join(evens))
+s=input()
+a=s.split()
+n=int(a[0])
+m=int(a[1])
+num=[str(x) for x in range(n+1,m) if(x%2==0)]
+print(" ".join(num))
