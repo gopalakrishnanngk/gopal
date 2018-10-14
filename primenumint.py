0c2ac97e2010610088d1260c8df2b9debd126cf7
gopalakrishnanngk <34596624+gopalakrishnanngk@users.noreply.github.com>
2018-10-14 22:37:01 +0530

    Update primenumint.py

--- primenumint.py
+++ primenumint.py
@@ -1,9 +1,15 @@
-a=1
-b=10
-for num in range (a,b+1):
+s=input()
+a=s.split()
+n=int(a[0])
+m=int(a[1])
+b=[]
+for num in range(n,m):
+	p=0
 	if num>1:
 		for i in range(2,num):
-			if(num%i)==0:
-			    break
-		else:
-			print(num)
+			if(num%i==0):
+				p=1
+		if(p==0):
+			b.append(str(num))
+print(" ".join(b))
+				
