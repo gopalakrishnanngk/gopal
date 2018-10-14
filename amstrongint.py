b78aae75537ca226d9a38b04ae1e216665315bce
gopalakrishnanngk <34596624+gopalakrishnanngk@users.noreply.github.com>
2018-10-14 23:30:27 +0530

    Update amstrongint.py

--- amstrongint.py
+++ amstrongint.py
@@ -1,12 +1,19 @@
-y=int(input())
-z=int(input())
-for num in range(y,z):
-	order = len(str(num))
-	sum = 0
-	temp = num
-	while temp > 0:
-		digit = temp % 10
-		sum += digit ** order
-		temp //= 10 
-	if num == sum:
-	    print(num)
+s=input()
+a=s.split()
+p=int(a[0])
+q=int(a[1])
+b=[]
+for num in range(p,q):
+	m=str(num)
+	n=len(m)
+	sum=0
+	for i in range(n):
+		c=m[i]
+		e=int(c)
+		d=e**n
+		sum=sum+d
+	if(sum==num):
+		b.append(m)
+print(" ".join(b))
+				
+	
