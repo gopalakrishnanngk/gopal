5576d255b6b74722ed834bfded7b79c05ac207d2
gopalakrishnanngk <34596624+gopalakrishnanngk@users.noreply.github.com>
2018-10-15 22:46:39 +0530

    Update fibona.py

--- fibona.py
+++ fibona.py
@@ -1,6 +1,9 @@
 n=int(input())
-a=['1','1']
-for i in range(2,n):
-	b=str(int(a[i-2])+int(a[i-1]))
-	a.append(b)
-print(" ".join(a))	
+if (n>1):
+	a=['1','1']
+	for i in range(2,n):
+		b=str(int(a[i-2])+int(a[i-1]))
+		a.append(b)
+	print(" ".join(a))	
+else:
+	print(1)
